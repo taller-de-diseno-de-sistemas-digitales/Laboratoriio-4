54f1ff4f7afb572f4702babb94904042671d4ae6
AllanCh <edochacon96@gmail.com>
2019-10-17 17:10:33 -0600

    Agregar modulo visibilidad sprites
    Se agrega modulo para controlar cuando un sprite debe ser visible

--- visibleLogic.sv
+++ visibleLogic.sv
@@ -1,10 +1,11 @@
 module visibleLogic (input xm63, ym63,
 							input [2:0] color,
+							input enable,
 							output visible);
 
 logic background;
 assign background = (color == 3'd0); 
-assign visible = ~background && ~(xm63 | ym63);
+assign visible = ~background && ~(xm63 | ym63) && enable;
 	
 endmodule
 //
